cc9aa4c9f442452bb5fcfaa1cdaa9a7180e19f2d
Agent-Bacon <ogbacon27@gmail.com>
2020-12-17 14:18:49 -0700

    tailwind css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's go to the park
         </p>
         <a
           className="App-link"
